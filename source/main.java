cd0aba2ec92c21328b96c9036baf25e19c214b78
pedroamaral06 <robot.knight0ne06@gmail.com>
2025-11-12 11:05:52 +0000

    Update main.java

--- source/main.java
+++ source/main.java
@@ -1,11 +1,7 @@
 //Exercício 2
 void main() {
-    int i = 5;
-    double f = 1.5f;
-    boolean b = true;
-    char c = 'A';
-    System.out.println("Número Inteiro: " + i);
-    System.out.println("Número Decimal: " + f);
-    System.out.println("Valor Booleano: " + b);
-    System.out.println("Carater: " + c);
+    double d = 5.9;
+    int i = (int) d;
+    System.out.println("Valor do Número Decimal: " + d);
+    System.out.println("Valor do Número Convertido para Inteiro: " + i);
 }
