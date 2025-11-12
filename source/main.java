7d8d5993012f86a3fb9f7542ecfe2bc42e3f55dd
pedroamaral06 <robot.knight0ne06@gmail.com>
2025-11-12 11:57:03 +0000

    Update main.java

--- source/main.java
+++ source/main.java
@@ -1,28 +1,30 @@
-//Exercício 6
+//Exercício 1
 import java.util.Scanner;
 void main() {
     Scanner scanner = new Scanner(System.in);
-    final int PIN_CORRETO = 1234;
-    int tentativas = 0;
-    boolean pinCorreto = false;
 
-    System.out.println("Introduza o seu PIN (máximo de 3 tentativas):");
+    System.out.print("Introduza o seu peso (kg): ");
+    double peso = scanner.nextDouble();
 
-    while (tentativas < 3 && !pinCorreto) {
-        System.out.print("Tentativa " + (tentativas + 1) + ": ");
-        int pin = scanner.nextInt();
+    System.out.print("Introduza a sua altura (m): ");
+    double altura = scanner.nextDouble();
 
-        if (pin == PIN_CORRETO) {
-            System.out.println("PIN CORRETO");
-            pinCorreto = true;
-        } else {
-            System.out.println("PIN ERRADO");
-            tentativas++;
-        }
-    }
+    double imc = peso / (altura * altura);
 
-    if (!pinCorreto) {
-        System.out.println("Demasiadas tentativas falhadas. Acesso bloqueado.");
+    System.out.printf("\nO seu IMC é: %.2f\n", imc);
+
+    if (imc < 18.5) {
+        System.out.println("Categoria: Abaixo do peso");
+    } else if (imc >= 18.5 && imc <= 24.9) {
+        System.out.println("Categoria: Peso ideal (parabéns)");
+    } else if (imc >= 25.0 && imc <= 29.9) {
+        System.out.println("Categoria: Levemente acima do peso");
+    } else if (imc >= 30.0 && imc <= 34.9) {
+        System.out.println("Categoria: Obesidade grau I");
+    } else if (imc >= 35.0 && imc <= 39.9) {
+        System.out.println("Categoria: Obesidade grau II (severa)");
+    } else {
+        System.out.println("Categoria: Obesidade grau III (mórbida)");
     }
 
     scanner.close();
