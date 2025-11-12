747997e455d61ea9ed94c0d69951b536a630b62f
pedroamaral06 <robot.knight0ne06@gmail.com>
2025-11-12 11:48:50 +0000

    Update main.java

--- source/main.java
+++ source/main.java
@@ -1,25 +1,31 @@
-//Exercício 3
+//Exercício 1
+import java.util.Scanner;
 void main() {
     Scanner scanner = new Scanner(System.in);
 
-    System.out.print("Nota do Teste A (40%): ");
-    double testeA = scanner.nextDouble();
+    System.out.print("Introduza o seu peso (kg): ");
+    double peso = scanner.nextDouble();
 
-    System.out.print("Nota do Teste B (40%): ");
-    double testeB = scanner.nextDouble();
+    System.out.print("Introduza a sua altura (m): ");
+    double altura = scanner.nextDouble();
 
-    System.out.print("Nota do Teste C (20%): ");
-    double testeC = scanner.nextDouble();
+    double imc = peso / (altura * altura);
 
-    double notaFinal = (testeA * 0.4) + (testeB * 0.4) + (testeC * 0.2);
-    double media = (testeA + testeB + testeC) / 3.0;
+    System.out.printf("\nO seu IMC é: %.2f\n", imc);
 
-    System.out.println("\n--- Resultados ---");
-    System.out.println("Nota do Teste A: " + testeA);
-    System.out.println("Nota do Teste B: " + testeB);
-    System.out.println("Nota do Teste C: " + testeC);
-    System.out.printf("Nota Final (com pesos): %.2f\n", notaFinal);
-    System.out.printf("Média Simples: %.2f\n", media);
+    if (imc < 18.5) {
+        System.out.println("Categoria: Abaixo do peso");
+    } else if (imc >= 18.5 && imc <= 24.9) {
+        System.out.println("Categoria: Peso ideal (parabéns)");
+    } else if (imc >= 25.0 && imc <= 29.9) {
+        System.out.println("Categoria: Levemente acima do peso");
+    } else if (imc >= 30.0 && imc <= 34.9) {
+        System.out.println("Categoria: Obesidade grau I");
+    } else if (imc >= 35.0 && imc <= 39.9) {
+        System.out.println("Categoria: Obesidade grau II (severa)");
+    } else {
+        System.out.println("Categoria: Obesidade grau III (mórbida)");
+    }
 
     scanner.close();
 }
