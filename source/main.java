6ce1f9569440840ae916e60516e2c9c6f19c47cf
pedroamaral06 <robot.knight0ne06@gmail.com>
2025-11-12 11:00:58 +0000

    Initial project setup and add main.java example
    Added IntelliJ IDEA project configuration files and a sample main.java demonstrating basic variable declarations and output. This sets up the project structure and provides a starting point for further development.

---
+++ source/main.java
@@ -0,0 +1,11 @@
+//Exercício 2
+void main() {
+    int i = 5;
+    double f = 1.5f;
+    boolean b = true;
+    char c = 'A';
+    System.out.println("Número Inteiro: " + i);
+    System.out.println("Número Decimal: " + f);
+    System.out.println("Valor Booleano: " + b);
+    System.out.println("Carater: " + c);
+}
