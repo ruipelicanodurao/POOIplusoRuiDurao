a8696e8251084a0e24db4911eef556f259ac2d2c
pedroamaral06 <robot.knight0ne06@gmail.com>
2025-11-12 11:15:18 +0000

    Replace arithmetic operations with Celsius to Fahrenheit conversion
    Removed previous integer arithmetic functions and input handling. Added a function to convert Celsius to Fahrenheit and updated main to demonstrate the conversion.

--- source/main.java
+++ source/main.java
@@ -1,31 +1,10 @@
-import java.util.Scanner;
-//Exercício 3
-
+//Exercício 5
 void main() {
-    int n1, n2;
-    Scanner myObj = new Scanner(System.in);
-    System.out.println("Introduza o Primeiro Número Inteiro: ");
-    n1 = myObj.nextInt();
-    System.out.println("Introduza o Segundo Número Inteiro: ");
-    n2 = myObj.nextInt();
-    System.out.println("A Soma dos Números Inteiros é: " + soma(n1,n2));
-    System.out.println("A Subtração dos Números Inteiros é: " + subtracao(n1,n2));
-    System.out.println("A Multiplicação dos Números Inteiros é: " + multiplicacao(n1,n2));
-    System.out.println("A Divisão dos Números Inteiros é: " + divisao(n1,n2));
+    double grausCelcius = 25.0;
+    double grausFahrenheit = ConversaoGrausCelcius(grausCelcius);
+    System.out.println(grausCelcius + " graus Celsius é igual a " + grausFahrenheit + " graus Fahrenheit.");
 }
 
-static int soma(int n1, int n2) {
-    return n1 + n2;
-}
-
-static int subtracao(int n1, int n2) {
-    return n1 - n2;
-}
-
-static int multiplicacao(int n1, int n2) {
-    return n1 * n2;
-}
-
-static int divisao(int n1, int n2) {
-    return n1 / n2;
+static double ConversaoGrausCelcius(double grausCelcius) {
+    return (grausCelcius * 9/5) + 32;
 }
